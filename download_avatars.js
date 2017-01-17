ed044e954604522f81b83357dcfb07d53bfc0a1b
ylhoony <yl.hoony@gmail.com>
2017-01-17 02:33:15 +0000

    Complete step 4, 5, 6'

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,39 @@
 var request = require('request');
 
 console.log('Welcome to the Github Avatar Downloader!');
+
+var GITHUB_USER = 'ylhoony';
+var GITHUB_TOKEN = 'a9d56634be25bbc6d33db7dfb478d9447a8c4491';
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+  // var requestURL = 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+  var requestOptions = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'Github Avatar Project'
+    },
+    bearer: GITHUB_TOKEN
+  }
+
+  // ...
+  request(requestOptions, function (err, response, body) {
+      var overallData = JSON.parse(body);
+
+      overallData.forEach(function(overallData) {
+
+        console.log("Avatar URL for ID " + overallData.id + " is " + overallData.avatar_url);
+
+      });
+
+  });
+
+        // .pipe(fs.createWriteStream('./future.jpg'));
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
