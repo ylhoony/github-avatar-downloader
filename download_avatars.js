957e2870fc1a9b911c34a7f9d2a519ee85b2fd47
ylhoony <yl.hoony@gmail.com>
2017-01-17 05:50:05 +0000

    add step 9

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,12 @@
 var request = require('request');
 var fs = require('fs');
-
+var repoOwner = process.argv[2];
+var repoName = process.argv[3];
 
 console.log('Welcome to the Github Avatar Downloader!');
 
 var GITHUB_USER = 'ylhoony';
 var GITHUB_TOKEN = 'a9d56634be25bbc6d33db7dfb478d9447a8c4491';
-
 
 var mkdirSync = function () {
   try {
@@ -17,11 +17,8 @@ var mkdirSync = function () {
   }
 }();
 
-
 function getRepoContributors(repoOwner, repoName, cb) {
-
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-
   var requestOptions = {
     url: requestURL,
     headers: {
@@ -29,38 +26,28 @@ function getRepoContributors(repoOwner, repoName, cb) {
     },
     bearer: GITHUB_TOKEN
   }
-
-   request(requestOptions, cb);
-
+   request(requestOptions, cb); // callback to getRepoContributor
 }
-
-
 
 function downloadImageByURL(url, filePath) {
-
   request.get(url)
-       .on('error', function (err) {
-          throw err;
-       })
-       .on('response', function(res) {
-          console.log('Downloading image...');
-          console.log('Download complete.');
-       })
-       .pipe(fs.createWriteStream('./avatar/' + filePath));
+         .on('error', function (err) {
+            throw err;
+         })
+         .on('response', function(res) {
+            console.log('Downloading image...');
+            console.log('Download complete.');
+         })
+         .pipe(fs.createWriteStream('./avatar/' + filePath));
 }
 
-
-
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors(repoOwner, repoName, function(err, result) {
   var parse = JSON.parse(result.body);
   parse.forEach(function(x){
   console.log(x.avatar_url);
   console.log(x.login);
-  console.log('------');
-//Calling function to download the images from the repository
-  downloadImageByURL(x.avatar_url, x.login +'.jpg' );
-  })
+  console.log('********');
 
-  // console.log("Errors:", err);
-  // console.log("Result:", result);
+  downloadImageByURL(x.avatar_url, x.login +'.jpg' ); // Call downloadImageByURL
+  })
 });
